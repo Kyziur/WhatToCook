2744c7f179200df885beb74177907f92359b858b
Bartosz Sitek <sitek.b7@gmail.com>
2023-01-28 16:23:34 +0100

    added nagiation bar with card components and menu list

--- src/WhatToCook.WebApp/ClientApp/src/app/shared/shared.module.ts
+++ src/WhatToCook.WebApp/ClientApp/src/app/shared/shared.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from '../shared/shared.component';
 import { FavouritesButtonComponent } from './favourites-button/favourites-button.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     SharedComponent,
-    FavouritesButtonComponent
+    FavouritesButtonComponent,
+    SearchComponent,
+    
   ],
   imports: [
     CommonModule
-  ]
+  ],
+exports: [FavouritesButtonComponent, SearchComponent],
 })
 export class SharedModule { }
