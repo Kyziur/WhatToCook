92387ab0437fba8f2aa08bdb97089fe31d19a005
Kyziur <krystian.sitek.94@gmail.com>
2023-01-31 18:59:15 +0100

    Cleaned up the code

--- src/WhatToCook.WebApp/ClientApp/src/app/layout/layout.component.ts
+++ src/WhatToCook.WebApp/ClientApp/src/app/layout/layout.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, fromEvent, map, Observable, of, startWith, tap, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
-  title = 'app';
+export class LayoutComponent implements OnInit {
+  public isNotMobile$: Observable<boolean> = of(false);
+  private isSidebarVisible = new BehaviorSubject<boolean>(false);
+  public isSidebarVisible$ = this.isSidebarVisible.asObservable();
+  ngOnInit(): void {
+    const checkScreenSize = () => document.body.offsetWidth > 767;
+    this.isNotMobile$ = fromEvent(window, 'resize').pipe(map(checkScreenSize), startWith(checkScreenSize()), tap(isNotMobile => {
+      if (isNotMobile) {
+        this.isSidebarVisible.next(false);
+      }
+    }));
 
+  }
 
+  showMenu() {
+    this.isSidebarVisible.next(!this.isSidebarVisible.value);
+  }
 }
