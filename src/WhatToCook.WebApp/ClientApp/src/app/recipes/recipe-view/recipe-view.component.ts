48191487c671b09f60cec64ed167d092740d3b1a
Kyziur <krystian.sitek.94@gmail.com>
2023-03-01 19:03:44 +0100

    Added readonly mode when displaying recipe

--- src/WhatToCook.WebApp/ClientApp/src/app/recipes/recipe-view/recipe-view.component.ts
+++ src/WhatToCook.WebApp/ClientApp/src/app/recipes/recipe-view/recipe-view.component.ts
@@ -1,7 +1,5 @@
-import {HttpClient} from '@angular/common/http';
-import {Component, Inject, OnInit, Type, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, FormBuilder, FormArray} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
 import {CreateRecipe} from './CreateRecipe';
 import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -16,24 +14,18 @@ import {NEVER, of, switchMap} from "rxjs";
 export class RecipeViewComponent implements OnInit {
   timeToPrepareOptions = ["Short", "Medium", "Long"];
   recipe?: Recipe;
+  recipeForm: FormGroup | null = null;
+  isEditable: boolean = false;
 
- 
-  constructor(private fb: FormBuilder, private recipeService: RecipeService, private router: Router, private route: ActivatedRoute,) {
+  constructor(private fb: FormBuilder, private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {
   }
 
   handleSuccesfulSave() {
     this.router.navigate(['recipes'])
   }
 
-  recipeForm = this.fb.group({
-    name: this.fb.nonNullable.control(''),
-    ingredients: this.fb.array([this.fb.nonNullable.control('')]),
-    preperationDescription: this.fb.nonNullable.control(''),
-    timeToPrepare: this.fb.nonNullable.control(''),
-  })
-
   get ingredientsControls(): FormArray<FormControl<string>> {
-    return this.recipeForm.get('ingredients') as FormArray<FormControl<string>>;
+    return this.recipeForm?.get('ingredients') as FormArray<FormControl<string>>;
   }
 
   preview: string = '';
@@ -45,25 +37,57 @@ export class RecipeViewComponent implements OnInit {
     //params examples:
     //query param: https://fancypage.com/recipe?name="pierogi"
     //route param: https://fancypage.com/recipe/pierogi
+    //route param: https://fancypage.com/recipe/create
 
     this.route.params.pipe(switchMap(params => {
+      console.error('params', params);
       const name = params['name']
       if(!name){
-        return NEVER;
+        return of(undefined);
       }
       return this.recipeService.getByName(name);
     })).subscribe(recipe => {
       this.recipe = recipe;
+      this.loadFormData(recipe);
       console.error('viewing recipe:', this.recipe);
     });
   }
 
-  save() {
-    this.recipeService.create(this.recipeForm.value as CreateRecipe).subscribe(x => this.handleSuccesfulSave())
+  enableEdit() {
+    this.isEditable = true;
+  }
+  save(){
+    //TODO: ADD REQUEST TO API TO UPDATE EDITED RECIPE
+    //AFTER SAVE RELOAD DATA
+    this.isEditable = false;
   }
 
+  submit() {
+    this.recipeService.create(this.recipeForm?.value as CreateRecipe).subscribe(x => this.handleSuccesfulSave())
+  }
 
   addIngredient() {
     this.ingredientsControls.push(this.fb.nonNullable.control(''));
+  }
+
+  loadFormData(recipe?: Recipe) {
+    const ingredientsControls = recipe?.ingredients.map(ingredient => {
+      return this.createStringControl(ingredient)
+    }) ?? [];
+
+    if(ingredientsControls.length === 0){
+      ingredientsControls.push(this.createStringControl(undefined))
+    }
+
+    this.recipeForm = this.fb.group({
+      name: this.createStringControl(recipe?.name),
+      ingredients: this.fb.array(ingredientsControls),
+      preparationDescription: this.createStringControl(recipe?.preparationDescription),
+      timeToPrepare: this.createStringControl(recipe?.timeToPrepare)
+    })
+  }
+
+  createStringControl(value: string | undefined) {
+    return this.fb.nonNullable.control(value ?? '');
   }
 }
