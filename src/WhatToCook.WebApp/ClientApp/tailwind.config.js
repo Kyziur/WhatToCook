b69ef47763c3700daab722940b37774b590c09ce
Kyziur <krystian.sitek.94@gmail.com>
2023-08-28 21:11:44 +0200

    UI Improvements

--- src/WhatToCook.WebApp/ClientApp/tailwind.config.js
+++ src/WhatToCook.WebApp/ClientApp/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
+  mode: 'jit',
   content: [
     "./src/**/*.{html,ts}",
+    "../src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: "class"
+    }),
+    require('@tailwindcss/typography'),
+  ],
 }
